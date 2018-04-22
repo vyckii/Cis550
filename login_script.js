7b7e8527e21fb1fb9f75e354932dfe71a5483753
NelsonCGY <flycloudwindmoon@hotmail.com>
2018-04-22 17:57:50 -0400

    empty input ok

--- login_script.js
+++ login_script.js
@@ -1,12 +1,15 @@
 var app = angular.module('Login', [])
 app.controller('logController', function($scope, $http) {
             $scope.Verify = function() {
-                console.log($scope.user_id + " " + $scope.user_pass)
+                if($scope.user_id === undefined || $scope.user_pass === undefined){
+                	alert('Invalid user id or password!')
+                	return
+                }
                 var request = $http.get("/login_info/" + $scope.user_id + "/" + $scope.user_pass)
                 request.success(function(data) {
                     if (data.length != 0) {
                         console.log(data)
-                        location.href="/search"
+                        location.href='/search'
                     } else {
                         alert('User not exist or wrong info!')
                     }
@@ -16,6 +19,10 @@ app.controller('logController', function($scope, $http) {
                 })
             }
             $scope.Create = function() {
+            	if($scope.user_id === undefined || $scope.user_pass === undefined){
+                	alert('Invalid user id or password!')
+                	return
+                }
                 var request = $http.get("/signup_check/" + $scope.user_id)
                 request.success(function(data) {
                     if (data.length != 0) {
@@ -25,7 +32,7 @@ app.controller('logController', function($scope, $http) {
                         request = $http.get("/signup_info/" + $scope.user_id + "/" + $scope.user_pass)
                         request.success(function(data) {
                             alert('Register success!')
-                            location.href="/search"
+                            location.href='/search'
                         })
                         request.error(function(data) {
                             console.log('err')
