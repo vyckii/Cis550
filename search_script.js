f19725dea4575370118c7dcfa9b07f046656239f
Shan Luo <shanluo@seas.upenn.edu>
2018-04-23 01:39:10 -0400

    Add files via upload

--- search_script.js
+++ search_script.js
@@ -15,3 +15,35 @@ app.controller('SearchController', function($scope, $http) {
     
     }; 
 });
+
+app.controller('NearMeController', function($scope, $http) {
+        $scope.Near = function() {
+        var request = $http.get("/search_near/" + $scope.addr + "/" + $scope.feature);
+        console.log($scope.city + " " + $scope.feature)
+        request.success(function(data) {
+            $scope.data = data;
+            console.log("get data " + data)
+            console.log("scope data" + $scope.data)
+        });
+        request.error(function(data){
+            console.log('err');
+        });
+    
+    }; 
+});
+
+app.controller('PopController', function($scope, $http) {
+        $scope.Pop = function() {
+        var request = $http.get("/search_pop/" + $scope.city + "/" + $scope.feature);
+        console.log($scope.city + " " + $scope.feature)
+        request.success(function(data) {
+            $scope.data = data;
+            console.log("get data " + data)
+            console.log("scope data" + $scope.data)
+        });
+        request.error(function(data){
+            console.log('err');
+        });
+    
+    }; 
+});
