666a55e007533ce59fd5c4042d4c7b43e99ddaa4
NelsonCGY <flycloudwindmoon@hotmail.com>
2018-04-23 12:06:48 -0400

    search anything!

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ var express = require('express')
 var app = express()
 var path = require('path')
 var oracledb = require('oracledb')
-var NodeGeocoder = require('node-geocoder');
-// var dbConfig = require('./dbconfig.js');
-oracledb.autoCommit = true;
+var NodeGeocoder = require('node-geocoder')
+oracledb.autoCommit = true
 var database
 oracledb.getConnection({
     user: "Group8",
@@ -28,23 +27,19 @@ oracledb.getConnection({
         })
 })
 var options = {
-  provider: 'google',
- 
-  // Optional depending on the providers
-  httpAdapter: 'https', // Default
-  apiKey: 'AIzaSyCeZQMIEHkPo_I0CQ9gAaV3Yf6juKQsbjw', // for Mapquest, OpenCage, Google Premier
-  formatter: null         // 'gpx', 'string', ...
-};
- 
-var geocoder = NodeGeocoder(options);
+    provider: 'google',
+    // Optional depending on the providers
+    httpAdapter: 'https', // Default
+    apiKey: 'AIzaSyCeZQMIEHkPo_I0CQ9gAaV3Yf6juKQsbjw', // for Mapquest, OpenCage, Google Premier
+    formatter: null // 'gpx', 'string', ...
+}
+var geocoder = NodeGeocoder(options)
 geocoder.geocode('29 champs elysée paris', function(err, res) {
-  console.log(res);
-});
-var lat = "";
-var long = "";
- 
+    console.log(res)
+})
+var lat = ""
+var long = ""
 // Using callback
-
 function doRelease(connection) {
     connection.close(function(err) {
         if (err) console.error(err.message)
@@ -54,10 +49,11 @@ app.set('port', (process.env.PORT || 5004))
 app.use(express.static(__dirname + '/public'))
 app.get('/quit', function(request, response) {
     doRelease(database)
+    process.exit()
 })
 app.get('/login', function(request, response) {
     response.sendFile(path.join(__dirname, '/', 'login.html'))
-}) 
+})
 app.get('/login_script.js', function(request, response) {
     response.sendFile(path.join(__dirname, '/', 'login_script.js'))
 })
@@ -102,29 +98,34 @@ app.get('/signup_info/:user_id/:password', function(request, response) {
         response.send(result.rows)
     })
 })
+app.get('/search_index', function(request, response) {
+    console.log('search')
+    response.sendFile(path.join(__dirname, '/', 'search_index.html'))
+})
+app.get('/search_script.js', function(request, response) {
+    response.sendFile(path.join(__dirname, '/', 'search_script.js'))
+})
 app.get('/search', function(request, response) {
     console.log('search')
     response.sendFile(path.join(__dirname, '/', 'search.html'))
 })
-
-app.get('/search_feature/:city/:feature/:time', function(request, response){
+app.get('/search_feature/:city/:feature/:time', function(request, response) {
     var city = request.params.city
     var feature = request.params.feature
     var time = request.params.time.toString()
     var cityQuery = ""
     var featureQuery = ""
     var timeQuery = ""
-
-    if(city != "undefined"){
+    if (city != "undefined") {
         cityQuery = "b.CITY = '" + city + "'"
     }
-    if(feature != "undefined"){
+    if (feature != "undefined") {
         featureQuery = "c.FEATURE = '" + feature + "'"
     }
-    if(time != "undefined"){
-        var myDate = new Date();
-        var days = ['MONDAY', 'TUESDAY','WEDNESDAY','THURSDAY','FRIDAY','SATURDAY','SUNDAY']
-        var today = days[myDate.getDay()];
+    if (time != "undefined") {
+        var myDate = new Date()
+        var days = ['MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY', 'SUNDAY']
+        var today = days[myDate.getDay()]
         var open = "h." + today + "_O"
         var close = "h." + today + "_C"
         timeQuery = open + " < " + "'" + time + "'" + " AND " + close + " > " + "'" + time + "'"
@@ -132,132 +133,101 @@ app.get('/search_feature/:city/:feature/:time', function(request, response){
     console.log(cityQuery)
     console.log(featureQuery)
     console.log(timeQuery)
-    
     //var query = "select b.NAME, b.ADDRESS, b.STARS, b.REVIEW_COUNT from BUSINESS b natural join CATEGORIES c\
-                 //where  c.FEATURE = '" + feature + "' and b.CITY = '" + city + "'" 
+    //where  c.FEATURE = '" + feature + "' and b.CITY = '" + city + "'" 
     var query = "select DISTINCT b.NAME, b.ADDRESS, b.STARS, b.REVIEW_COUNT from BUSINESS b natural join CATEGORIES c natural join HOUR h "
-    if(cityQuery != "" || featureQuery != "" || timeQuery != ""){
+    if (cityQuery != "" || featureQuery != "" || timeQuery != "") {
         query = query + "WHERE "
     }
     var prev = 0
-    if(cityQuery != ""){
+    if (cityQuery != "") {
         query = query + cityQuery
-        prev = prev | 1 
+        prev = prev | 1
     }
-    if(featureQuery != ""){
-        if(prev == 1){
+    if (featureQuery != "") {
+        if (prev == 1) {
             query = query + " AND "
         }
         prev = prev | 1
-        query = query + featureQuery;
+        query = query + featureQuery
     }
-    if(timeQuery != ""){
-        if(prev == 1){
+    if (timeQuery != "") {
+        if (prev == 1) {
             query = query + " AND "
         }
         prev = prev | 1
-        query = query + timeQuery;
+        query = query + timeQuery
     }
     console.log(query)
-
     database.execute(query, function(error, result) {
         if (error) {
             throw error
             return
         }
-        console.log(result.rows)
+        console.log("Get " + (result.rows).length + " results.")
         response.send(result.rows)
     })
 })
-app.get('/search_script.js', function(request, response) {
-    response.sendFile(path.join(__dirname, '/', 'search_script.js'))
-})
-
-app.get('/search_index', function(request, response) {
-    console.log('search')
-    response.sendFile(path.join(__dirname, '/', 'search_index.html'))
-})
-
 app.get('/near', function(request, response) {
     console.log('search_near ')
     response.sendFile(path.join(__dirname, '/', 'near_me.html'))
 })
-
-app.get('/search_near/:addr/:feature', function(request, response){
-    var addr = request.params.addr;
+app.get('/search_near/:addr/:feature', function(request, response) {
+    var addr = request.params.addr
     console.log(addr)
-    var feature = request.params.feature;
-    getAddr(addr);
-    console.log("long+:"+long)
-    getAddr(addr);
-    setTimeout(function(){
-    runquery(feature,response);
-    }, 5000);
-    
-
-});
+    var feature = request.params.feature
+    getAddr(addr)
+    setTimeout(function() {
+        runNear(feature, response)
+    }, 5000)
+})
 
 function getAddr(addr) {
     // stuff you want to happen right away
-    console.log('Welcome to My Console,');
+    console.log('Finding location for you...')
     geocoder.geocode(addr, function(err, res) {
-        lat = res[0].latitude;
-        long = res[0].longitude;
-    });
+        lat = res[0].latitude
+        long = res[0].longitude
+    })
 }
 
-function runquery(feature,response){
-    var query = "select DISTINCT b.NAME, b.ADDRESS, b.STARS, b.REVIEW_COUNT from BUSINESS b natural join CATEGORIES c WHERE (b.LAT -(" + lat + ")) * (b.LAT -(" + lat + ")) + (b.LON -(" + long + ")) * (b.LON -(" + long + ")) < 3 AND c.FEATURE = '" + feature + "'";
-
+function runNear(feature, response) {
+    var query = "select DISTINCT b.NAME, b.ADDRESS, b.STARS, b.REVIEW_COUNT from BUSINESS b natural join CATEGORIES c WHERE (b.LAT -(" + lat + ")) * (b.LAT -(" + lat + ")) + (b.LON -(" + long + ")) * (b.LON -(" + long + ")) < 3"
+    if (feature != "undefined") {
+        query = query + "AND c.FEATURE = '" + feature + "'"
+    }
     console.log(query)
-
     database.execute(query, function(error, result) {
         if (error) {
             throw error
             return
         }
-        console.log(result.rows)
+        console.log("Get " + (result.rows).length + " results.")
         response.send(result.rows)
     })
 }
-// app.get('/searchbest/:city', function(request, response){
-//     var city = request.params.city
-//     var query = "select NAME, LAT, LOG, STARS, REVIEW_COUNT from BUSINESS \
-//                  where STARS = (select max(b.from BUSINESS b \
-//                                 group by b.STATE \
-//                                 HAVING b.city = '" + city + "') AND CITY = '"  + city + "'"
-//     database.execute(query, function(error, result) {
-//         if (error) {
-//             throw error
-//             return
-//         }
-//         console.log(result.rows)
-//         response.send(result.rows)
-//     })
-// })
 app.get('/popular', function(request, response) {
     console.log('pop')
     response.sendFile(path.join(__dirname, '/', 'popular.html'))
 })
-
-app.get('/search_pop/:city/:feature', function(request, response){
-    var city = request.params.city;
+app.get('/search_pop/:city/:feature', function(request, response) {
+    var city = request.params.city
     console.log(city)
-    var feature = request.params.feature;
-    var query = "select DISTINCT b.NAME, b.ADDRESS, b.STARS, b.REVIEW_COUNT from BUSINESS b natural join CATEGORIES c WHERE c.FEATURE = '" + feature + "' AND b.CITY = '" + city + "' AND b.REVIEW_COUNT > 100 AND b.STARS = 5"
+    var feature = request.params.feature
+    var query = "select DISTINCT b.NAME, b.ADDRESS, b.STARS, b.REVIEW_COUNT from BUSINESS b natural join CATEGORIES c WHERE b.CITY = '" + city + "' AND b.REVIEW_COUNT > 100 AND b.STARS = 5"
+    if (feature != "undefined") {
+        query = query + "AND c.FEATURE = '" + feature + "'"
+    }
     console.log(query)
     database.execute(query, function(error, result) {
         if (error) {
             throw error
             return
         }
-        console.log(result.rows)
+        console.log("Get " + (result.rows).length + " results.")
         response.send(result.rows)
     })
-    
-
-});
-
+})
 app.listen(app.get('port'), function() {
     console.log("Node app is running at localhost:" + app.get('port'))
 })
